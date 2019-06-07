17d24dcc5efc71a81415c51cfdbff1ad177c556b
Paulo Lopes <pmlopes@gmail.com>
2019-06-07 11:37:07 +0200

    Update slidedeck

--- 10-things-js/_assets/theme/custom.js
+++ 10-things-js/_assets/theme/custom.js
@@ -1,4 +1,4 @@
 var watermark = document.createElement('div');
 watermark.id = 'watermark';
-watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Fira Code\'">@pml0pes</code>';
+watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Roboto Mono\'">@pml0pes</code>';
 document.getElementsByTagName('body')[0].appendChild(watermark);
