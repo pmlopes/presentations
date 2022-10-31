7a2a02766bdebb7b6a609069e0b3ba27fd2d3d5d
Paulo Lopes <pmlopes@gmail.com>
2022-10-31 23:18:18 +0100

    Adding sinf-2022
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- 10-things-js/_assets/theme/custom.js
+++ 10-things-js/_assets/theme/custom.js
@@ -1,4 +1,4 @@
 var watermark = document.createElement('div');
 watermark.id = 'watermark';
-watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Roboto Mono\'">@pml0pes</code>';
+watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Roboto Mono\'">@pml0p35</code>';
 document.getElementsByTagName('body')[0].appendChild(watermark);
--- 10-things-js/theme/custom.js
+++ 10-things-js/theme/custom.js
@@ -1,4 +1,4 @@
 var watermark = document.createElement('div');
 watermark.id = 'watermark';
-watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Fira Code\'">@pml0pes</code>';
+watermark.innerHTML = '<i class="fab fa-twitter"></i> <code style="font-family: \'Fira Code\'">@pml0p35</code>';
 document.getElementsByTagName('body')[0].appendChild(watermark);
